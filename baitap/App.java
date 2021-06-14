9f5ebbd29a9f7f62f01dee642297dfd3a9cfc057
levanphu1412 <levanphu5555@gmail.com>
2021-06-14 19:02:07 +0700

    baitap

--- baitap/App.java
+++ baitap/App.java
@@ -2,13 +2,16 @@ package baitap;
 
 public class App {
     public static void main(String[] args) {
-        Palindrome palindrome = new Palindrome();
-        palindrome.palindrome();
-        GuessNumber guessNumber = new GuessNumber();
+        // Palindrome palindrome = new Palindrome();
+        // palindrome.palindrome();
+        // GuessNumber guessNumber = new GuessNumber();
         // guessNumber.guessNumber();
-        BMI bmi = new BMI();
-        // bmi.guessBMI();
-        GuessEmail guessEmail = new GuessEmail();
-        guessEmail.guessEmail();
+        // BMI bmi = new BMI();
+        // // bmi.guessBMI();
+        // GuessEmail guessEmail = new GuessEmail();
+        // guessEmail.guessEmail();
+        // // loc tu bay
+        LocTuBay loc = new LocTuBay();
+        loc.locTuBay(); 
     }
 }
