7d14a99a994a8c09af97e965f60f84980bfa716c
levanphu1412 <levanphu5555@gmail.com>
2021-06-29 01:00:40 +0700

    json

---
+++ ReadJSON/CustomerManager/src/App.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ReadFile readFile = new ReadFile();
+        // readFile.getData();
+
+        // System.out.println(readFile.getCustomers());
+        // readFile.getCustomers();
+
+        readFile.getPerson();
+
+        
+        
+
+        
+    }
+}
