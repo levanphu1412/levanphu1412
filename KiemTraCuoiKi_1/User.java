00e48f9659e460dbab62c3d5b91b3b174a8b8324
levanphu1412 <levanphu5555@gmail.com>
2021-07-24 22:49:24 +0700

    login

---
+++ KiemTraCuoiKi_1/User.java
@@ -0,0 +1,64 @@
+package KiemTraCuoiKi_1;
+
+/**
+ * User
+ */
+public class User {
+
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = Validate.validateEmail(email);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = Validate.validatePassword(password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
